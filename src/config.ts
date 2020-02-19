d3719c46a323e0fd94eb707573e59a2936c7df19
SteveHicksCH <shicks@companieshouse.gov.uk>
2020-02-19 16:02:24 +0000

    use new environmental variable for internal

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import ApplicationConfiguration from "./ApplicationConfiguration";
 const config: ApplicationConfiguration = {
     port: parseInt(process.env.RESTRICTED_WORD_WEB_PORT as string),
     apiAddress: process.env.INTERNAL_API_URL as string,
-    internalApiKey: process.env.CHS_API_KEY as string,
+    internalApiKey: process.env.CHS_INTERNAL_API_KEY as string,
     env: (process.env.NODE_ENV || "development").toLowerCase(),
     urlPrefix: "restricted-word"
 };
