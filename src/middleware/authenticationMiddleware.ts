56a8ed296da751362c0b85e15cbb512bf32fe69d
SteveHicksCH <shicks@companieshouse.gov.uk>
2020-03-16 12:55:34 +0000

    correct use of session library

--- src/middleware/authenticationMiddleware.ts
+++ src/middleware/authenticationMiddleware.ts
@@ -3,29 +3,40 @@ import { RequestHandler } from "express";
 import { Session } from "ch-node-session-handler";
 import { SessionKey } from "ch-node-session-handler/lib/session/keys/SessionKey";
 import { SignInInfoKeys } from "ch-node-session-handler/lib/session/keys/SignInInfoKeys";
+import { UserProfileKeys } from "ch-node-session-handler/lib/session/keys/UserProfileKeys";
 import config from "../config";
 import { createLogger } from "ch-structured-logging";
 
 const logger = createLogger(config.applicationNamespace);
 
 const createAuthenticationMiddleware = function (): RequestHandler {
-
     return (request, response, next) => {
+        // request.session.ifNothing(() => {
+        //     logger.errorRequest(request, "Session doesn't exist");
 
-        request.session.ifNothing(() => {
+        //     return response.redirect(`/signin?return_to=/${config.urlPrefix}`);
+        // });
 
-            logger.errorRequest(request, "Session doesn't exist");
+        console.log("headers", request.headers);
 
-            return next();
-        });
+        const signInInfo: ISignInInfo | undefined = request.session
+            .chain((session: Session) => session.getValue<ISignInInfo>(SessionKey.SignInInfo)).extract();
 
-        const signedIn = request.session
-            .chain((session: Session) => session.getValue<ISignInInfo>(SessionKey.SignInInfo))
-            .map((signInInfo: ISignInInfo) => signInInfo[SignInInfoKeys.SignedIn] === 1)
-            .orDefault(false);
+        if (signInInfo !== undefined) {
+            const signedIn = signInInfo[SignInInfoKeys.SignedIn] === 1;
+            const userInfo: any = signInInfo[SignInInfoKeys.UserProfile];
 
-        if (signedIn) {
-            return next();
+            if (userInfo !== undefined) {
+                const permissions = userInfo[UserProfileKeys.Permissions] as any;
+                const scope = userInfo[UserProfileKeys.Scope];
+
+                console.log("permissions is", permissions);
+                console.log("scope is", scope);
+
+                if (signedIn) {
+                    return next();
+                }
+            }
         }
 
         return response.redirect(`/signin?return_to=/${config.urlPrefix}`);
