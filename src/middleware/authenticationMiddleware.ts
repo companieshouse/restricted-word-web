96f41f601eb36ca2f292a66669bd25ec2a2d1fd2
csciubbach <csciubba@companieshouse.gov.uk>
2020-03-16 18:20:52 +0000

    Added console logs

--- src/middleware/authenticationMiddleware.ts
+++ src/middleware/authenticationMiddleware.ts
@@ -17,14 +17,20 @@ const createAuthenticationMiddleware = function (): RequestHandler {
             .chain((session: Session) => session.getValue<ISignInInfo>(SessionKey.SignInInfo))
             .extract();
 
+        console.log("signInInfo", signInInfo);
+
         if (signInInfo !== undefined) {
 
             const signedIn = signInInfo[SignInInfoKeys.SignedIn] === 1;
             const userInfo = signInInfo[SignInInfoKeys.UserProfile];
 
+            console.log("userInfo", userInfo);
+
             if (signedIn && userInfo !== undefined) {
 
                 const permissions = userInfo[UserProfileKeys.Permissions];
+
+                console.log("permissions", permissions);
 
                 if (permissions !== undefined && permissions["/admin/restricted-word"] === 1) {
                     return next();
