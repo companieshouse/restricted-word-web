433220cdbe8c766a0ff0a45b14883cddf27d69b1
tsciubbach <tsciubba@companieshouse.gov.uk>
2021-07-14 10:26:13 +0100

    Restored fix changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const gulp = require("gulp");
-const sass = require("gulp-sass")(require("node-sass"));
+const sass = require("gulp-sass");
+
+sass.compiler = require("node-sass");
 
 // Copies the fonts and images from the govuk-frontend package to the dist directory
 gulp.task("govuk-frontend-copy", function () {
