a1551f591f512f4c990a473d31037d1d257ba11d
donmamu2P0 <bboxwalla@companieshouse.gov.uk>
2023-10-29 13:00:53 +0000

    Added tags to summary list

--- src/helpers/word.ts
+++ src/helpers/word.ts
@@ -1,14 +1,24 @@
-const mapIdToCategory = new Map<String, String>();
-mapIdToCategory.set("restricted", "Restricted");
-mapIdToCategory.set("criminal-fraudulent-purposes", "Names for criminal / fraudulent purposes");
-mapIdToCategory.set("international-orgs-foreign-gov-depts", "International organisations and foreign government departments");
-mapIdToCategory.set("prev-subjected-to-direction-to-change", "Names previously subjected to a direction to change them");
+const mapIdToCategory: any = {
+    "restricted": ["Restricted", "Restricted", "govuk-tag govuk-tag--red"],
+    "criminal-fraudulent-purposes": ["Names for criminal / fraudulent purposes", "Crimes/Fraud", "govuk-tag govuk-tag--blue"],
+    "international-orgs-foreign-gov-depts": ["International organisations and foreign government departments", "Int'l orgs/govts", "govuk-tag govuk-tag--green"],
+    "prev-subjected-to-direction-to-change": ["Names previously subjected to a direction to change them", "Dir. to change", "govuk-tag govuk-tag--yellow"]
+}
 
-function getCategoriesListHtml (categories: String[]): String {
-    let categoriesListHtml: String = ""
+function getCategoriesListHtml (categories: string[]): string {
+    let categoriesListHtml: string = ""
     if (categories.length > 0) {
         for (let i = 0 ; i < categories.length ; i++) {
-            categoriesListHtml += mapIdToCategory.get(categories[i]) + "<br>"
+            const categoryDetails: string[] = mapIdToCategory[categories[i]];
+            categoriesListHtml += `
+                <div class=\"tooltip\">
+                    <strong class=\"${categoryDetails[2]}\">
+                        ${categoryDetails[1]}
+                    </strong>
+                    <span class=\"tooltip-text\">${categoryDetails[0]}</span>
+                </div>
+                <br>
+            `
         }
     }
     return categoriesListHtml
--- test/helpers/word.test.ts
+++ test/helpers/word.test.ts
@@ -11,7 +11,16 @@ describe("RestrictedWordHelper", function() {
         });
 
         it("returns proper html when called with one category", async function () {
-            expect(getCategoriesListHtml(['criminal-fraudulent-purposes'])).to.equal("Names for criminal / fraudulent purposes" + "<br>")
+            expect(getCategoriesListHtml(['criminal-fraudulent-purposes']))
+                .to.equal(`
+                <div class="tooltip">
+                    <strong class="govuk-tag govuk-tag--blue">
+                        Crimes/Fraud
+                    </strong>
+                    <span class="tooltip-text">Names for criminal / fraudulent purposes</span>
+                </div>
+                <br>
+            `)
         });
 
         it("returns proper html when called with multiple category", async function () {
@@ -21,11 +30,31 @@ describe("RestrictedWordHelper", function() {
                     'criminal-fraudulent-purposes',
                     'prev-subjected-to-direction-to-change'
                 ]))
-                .to.equal(
-                    "Restricted" + "<br>" +
-                    "Names for criminal / fraudulent purposes" + "<br>" +
-                    "Names previously subjected to a direction to change them" + "<br>"
-                )
+                .to.equal(`
+                <div class="tooltip">
+                    <strong class="govuk-tag govuk-tag--red">
+                        Restricted
+                    </strong>
+                    <span class="tooltip-text">Restricted</span>
+                </div>
+                <br>
+            
+                <div class="tooltip">
+                    <strong class="govuk-tag govuk-tag--blue">
+                        Crimes/Fraud
+                    </strong>
+                    <span class="tooltip-text">Names for criminal / fraudulent purposes</span>
+                </div>
+                <br>
+            
+                <div class="tooltip">
+                    <strong class="govuk-tag govuk-tag--yellow">
+                        Dir. to change
+                    </strong>
+                    <span class="tooltip-text">Names previously subjected to a direction to change them</span>
+                </div>
+                <br>
+            `)
         });
     });
 });
