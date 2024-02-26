16b9b7fe7771d1058505a3a53aa2d3087af1e662
Harish Marimuthu <hmarimuthu@companieshouse.gov.uk>
2024-02-26 16:01:04 +0000

    CC-1189 Terraform ECS changes, env file changes

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -2,56 +2,69 @@
 locals {
   stack_name                = "search-service" # this must match the stack name the service deploys into
   name_prefix               = "${local.stack_name}-${var.environment}"
+  global_prefix             = "global-${var.environment}"
   service_name              = "restricted-word-web"
   container_port            = "3000" # default node port required here until prod docker container is built allowing port change via env var
   docker_repo               = "restricted-word-web"
+  kms_alias                 = "alias/${var.aws_profile}/environment-services-kms"
   lb_listener_rule_priority = 96
   lb_listener_paths         = ["/admin/restricted-word","/admin/restricted-word/*"]
   healthcheck_path          = "/admin/restricted-word/healthcheck" #healthcheck path for restricted word web
   healthcheck_matcher       = "200"
-
-  kms_alias                 = "alias/${var.aws_profile}/environment-services-kms"
-  service_secrets           = jsondecode(data.vault_generic_secret.service_secrets.data_json)
-
-  parameter_store_secrets    = {
-    "vpc_name"                  = local.service_secrets["vpc_name"]
-    "internal_api_url"          = local.service_secrets["internal_api_url"]
-    "cache_server"              = local.service_secrets["cache_server"]
-    "chs_internal_api_key"      = local.service_secrets["chs_internal_api_key"]
-  }
-
   vpc_name                  = local.service_secrets["vpc_name"]
-  internal_api_url          = local.service_secrets["internal_api_url"]
-  cache_server              = local.service_secrets["cache_server"]
-  chs_internal_api_key      = local.service_secrets["chs_internal_api_key"]
+  application_subnet_ids      = data.aws_subnets.application.ids
+  application_subnet_pattern  = local.stack_secrets["application_subnet_pattern"]
 
-  # create a map of secret name => secret arn to pass into ecs service module
-  # using the trimprefix function to remove the prefixed path from the secret name
-  secrets_arn_map = {
-    for sec in data.aws_ssm_parameter.secret:
-      trimprefix(sec.name, "/${local.name_prefix}/") => sec.arn
+  # Environment Files
+  use_set_environment_files   = var.use_set_environment_files
+  app_environment_filename    = "lfp-appeals-frontend.env"
+
+  # Secrets
+  stack_secrets               = jsondecode(data.vault_generic_secret.stack_secrets.data_json)
+  service_secrets             = jsondecode(data.vault_generic_secret.service_secrets.data_json)
+
+  # GLOBAL: create a map of secret name => secret arn to pass into ecs service module
+  global_secrets_arn_map = {
+    for sec in data.aws_ssm_parameter.global_secret :
+    trimprefix(sec.name, "/${local.global_prefix}/") => sec.arn
   }
 
+  # GLOBAL: create a list of secret name => secret arn to pass into ecs service module
+  global_secret_list = flatten([for key, value in local.global_secrets_arn_map :
+    { "name" = upper(key), "valueFrom" = value }
+  ])
+
+  # SERVICE: create a map of secret name => secret arn to pass into ecs service module
   service_secrets_arn_map = {
-    for sec in module.secrets.secrets:
-      trimprefix(sec.name, "/${local.service_name}-${var.environment}/") => sec.arn
+    for sec in module.secrets.secrets :
+    trimprefix(sec.name, "/${local.service_name}-${var.environment}/") => sec.arn
   }
 
-  task_secrets = [
-    { "name": "VPC_NAME", "valueFrom": "${local.service_secrets_arn_map.vpc_name}" },
-    { "name": "COOKIE_SECRET", "valueFrom": "${local.secrets_arn_map.web-oauth2-cookie-secret}" },
-    { "name": "CHS_INTERNAL_API_KEY", "valueFrom": "${local.service_secrets_arn_map.chs_internal_api_key}" },
-    { "name": "CACHE_SERVER", "valueFrom": "${local.service_secrets_arn_map.cache_server}" },
-    { "name": "INTERNAL_API_URL", "valueFrom": "${local.service_secrets_arn_map.internal_api_url}" }
+  # SERVICE: create a list of secret name => secret arn to pass into ecs service module
+  service_secret_list = flatten([for key, value in local.service_secrets_arn_map :
+    { "name" = upper(key), "valueFrom" = value }
+  ])
+
+  # TASK SECRET: GLOBAL SECRET + SERVICE SECRET
+  task_secrets = concat(local.global_secret_list,local.service_secret_list,[
+  ])
+
+  # GLOBAL: create a map of secret name and secret version to pass into ecs service module
+  ssm_global_version_map = [
+    for sec in data.aws_ssm_parameter.global_secret : {
+      name = "GLOBAL_${var.ssm_version_prefix}${replace(upper(basename(sec.name)), "-", "_")}", value = sec.version
+    }
   ]
 
-  task_environment = [
-    { "name": "CHS_URL", "value": "${var.chs_url}" },
-    { "name": "COOKIE_DOMAIN", "value": "${var.cookie_domain}" },
-    { "name": "COOKIE_NAME", "value": "${var.cookie_name}" },
-    { "name": "LOG_LEVEL", "value": "${var.log_level}" },
-    { "name": "NODE_ENV", "value": "${var.node_env}" },
-    { "name": "RESTRICTED_WORD_WEB_PORT", "value": "${local.container_port}" },
-    { "name": "SECURE_COOKIE", "value": "${var.secure_cookie}" }
+  # SERVICE: create a map of secret name and secret version to pass into ecs service module
+  ssm_service_version_map = [
+    for sec in module.secrets.secrets : {
+      name = "${replace(upper(local.service_name), "-", "_")}_${var.ssm_version_prefix}${replace(upper(basename(sec.name)), "-", "_")}", value = sec.version
+    }
   ]
+
+  # TASK ENVIRONMENT: GLOBAL SECRET Version + SERVICE SECRET Version
+  task_environment = concat(local.ssm_global_version_map,local.ssm_service_version_map,[
+    { "name" : "RESTRICTED_WORD_WEB_PORT", "value" : "${local.container_port}" }
+  ])
 }
