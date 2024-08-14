65035bfe6ae1f3cedc952bc89875d6a8a2077f53
nawazish587 <nali@companieshouse.gov.uk>
2024-08-14 16:31:59 +0100

    Corrected format for code
     On branch feature/sec-116-fix-csrf-issue
     Your branch is up to date with 'origin/feature/sec-116-fix-csrf-issue'.
    
     Changes to be committed:
    	modified:   src/middleware/createCsrfErrorMiddleware.ts

--- src/middleware/createCsrfErrorMiddleware.ts
+++ src/middleware/createCsrfErrorMiddleware.ts
@@ -3,18 +3,19 @@ import { CsrfError } from "@companieshouse/web-security-node";
 
 const csrfErrorTemplateName = "403";
 
-const csrfErrorHandler = (err: CsrfError | Error, _: Request,
-  res: Response, next: NextFunction) => {
-
+const csrfErrorHandler = (
+  err: CsrfError | Error,
+  _: Request,
+  res: Response,
+  next: NextFunction
+) => {
   if (!(err instanceof CsrfError)) {
     return next(err);
   }
 
-  return res.status(403).render(
-    csrfErrorTemplateName, {
-    csrfErrors: true
-  }
-  )
+  return res.status(403).render(csrfErrorTemplateName, {
+    csrfErrors: true,
+  });
 };
 
 export default csrfErrorHandler;
