994b53eb9f34436359fbb021fc613dc1ca48f42d
sunilmurugesan <smurugesan@companieshouse.gov.uk>
2025-06-11 16:06:21 +0100

    CC-2096 - Auto instrumentation for restricted-word-web (Node)

--- src/ins.ts
+++ src/ins.ts
@@ -2,10 +2,7 @@ import { NodeSDK } from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto';
-import {
-  PeriodicExportingMetricReader
-} from '@opentelemetry/sdk-metrics';
-
+import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 const sdk = new NodeSDK({
   traceExporter: new OTLPTraceExporter({
     url: 'http://host.docker.internal:4318/v1/traces',
@@ -19,5 +16,6 @@ const sdk = new NodeSDK({
   }),
   instrumentations: [getNodeAutoInstrumentations()],
 });
+console.log(sdk);
 
 sdk.start();
