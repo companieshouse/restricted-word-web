e83afe28880230bbfa9b6bd9c7020516f41e0868
Harish Marimuthu <hmarimuthu@companieshouse.gov.uk>
2025-06-20 10:30:17 +0100

    CC-2110 Amending AWS Cloudwatch log.

--- terraform/groups/ecs-service/cloudwatch.tf
+++ terraform/groups/ecs-service/cloudwatch.tf
@@ -1,9 +1,9 @@
 resource "aws_cloudwatch_log_group" "otel_logs" {
-  name              = "/ecs/search-service-cidev/otel/restricted-word-web-log"
+  name              = "/ecs/search-service-cidev/restricted-word-web-log"
   retention_in_days = var.otel_log_group_retention_in_days
 }
 
 resource "aws_cloudwatch_log_stream" "log_stream" {
-  name           = "testing-integrations-stream"
+  name           = "otel-integrations-stream"
   log_group_name = aws_cloudwatch_log_group.otel_logs.name
 }
