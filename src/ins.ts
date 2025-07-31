3ca567f73af683e15b52eb48131840908d576436
sunilmurugesan <smurugesan@companieshouse.gov.uk>
2025-07-31 09:22:20 +0100

    CC-2579 - Baggage PoC - Code Tidy-up.

--- src/ins.ts
+++ src/ins.ts
@@ -1,24 +1,20 @@
-import { NodeSDK, tracing } from "@opentelemetry/sdk-node";
+import { NodeSDK } from "@opentelemetry/sdk-node";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
 import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-proto";
 import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
 import config from "./config";
 import { ALLOW_ALL_BAGGAGE_KEYS, BaggageSpanProcessor } from "@opentelemetry/baggage-span-processor";
-import { BatchSpanProcessor, ConsoleSpanExporter, SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
-import { propagation } from "@opentelemetry/api";
-import { CompositePropagator, W3CBaggagePropagator, W3CTraceContextPropagator } from '@opentelemetry/core';
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-node";
 
 const traceExporter = new OTLPTraceExporter({
     url: config.traceExporterUrl,
     headers: {}
 });
-
 const sdk = new NodeSDK({
     spanProcessors: [
         new BaggageSpanProcessor(ALLOW_ALL_BAGGAGE_KEYS),
-        new BatchSpanProcessor(traceExporter),
-        new BatchSpanProcessor(new ConsoleSpanExporter())
+        new BatchSpanProcessor(traceExporter)
     ],
 
     metricReader: new PeriodicExportingMetricReader({
