f26c63ccbd93a17992f5b5551f5fcddee421a9b7
Harish Marimuthu <hmarimuthu@companieshouse.gov.uk>
2025-09-23 10:46:22 +0100

    CC-2096 Removing feature flag branch reference.

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -30,7 +30,7 @@ module "secrets" {
 }
 
 module "ecs-service" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.346"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.346"
             
   # Environmental configuration
   environment             = var.environment
