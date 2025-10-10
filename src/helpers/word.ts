29f47acec0819b2d3c3c8777dcc0336120dcc6e9
sunilmurugesan <37124115+sunilmurugesan@users.noreply.github.com>
2025-10-10 16:13:57 +0100

    ASM-721 - Fix vulnerabilities (#173)
    * ASM-721 - Added Nodemon to enable hot reloading during local development.
    
    * ASM-721 - Fix dependency-check issues.
    
    * ASM-721 - Fix lint issues.
    
    * ASM-721 - Fix dependency-check issues.
    
    * ASM-721 - Fix security hotspots flagged up in Sonar.
    
    * ASM-721 - Fix security hotspots flagged up in Sonar and add unit test.
    
    * ASM-721 - Fix lint issues.

--- src/helpers/word.ts
+++ src/helpers/word.ts
@@ -1,24 +1,38 @@
-const mapIdToCategory: any = {
+const mapIdToCategory: Record<string, string[]> = {
     restricted: ["Restricted", "Restricted", "govuk-tag govuk-tag--red"],
     "criminal-fraudulent-purposes": ["Names for criminal / fraudulent purposes", "Crimes/Fraud", "govuk-tag govuk-tag--blue"],
     "international-orgs-foreign-gov-depts": ["International organisations and foreign government departments", "Int'l orgs/govts", "govuk-tag govuk-tag--green"],
     "prev-subjected-to-direction-to-change": ["Names previously subjected to a direction to change them", "Dir. to change", "govuk-tag govuk-tag--yellow"]
 };
 
-function getCategoriesListHtml(categories: string[]): string {
+const htmlEscapeMap: Record<string, string> = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    "\"": "&quot;",
+    "'": "&#39;"
+};
+
+function getCategoriesListHtml(categories: string[], mappedCategoryOverride?: Record<string, string[]>): string {
+    // Protects against accidental inclusion of HTML tags by escaping
+    const escapeHtml = (str: string): string => str.replace(/[&<>"']/g, tag => htmlEscapeMap[tag] ?? tag);
+
     let categoriesListHtml: string = "";
     if (categories.length > 0) {
         for (const category of categories) {
-            const categoryDetails: string[] = mapIdToCategory[category];
-            categoriesListHtml += `
+            const categoryDetails: string[] = (mappedCategoryOverride ?? mapIdToCategory)[category];
+            if (categoryDetails) {
+                const [tooltipText, label, cssClass] = categoryDetails.map(escapeHtml);
+                categoriesListHtml += `
                 <div class="tooltip">
-                    <strong class="${categoryDetails[2]}">
-                        ${categoryDetails[1]}
+                    <strong class="${cssClass}">
+                        ${label}
                     </strong>
-                    <span class="tooltip-text">${categoryDetails[0]}</span>
+                    <span class="tooltip-text">${tooltipText}</span>
                 </div>
                 <br>
             `;
+            }
         }
     }
     return categoriesListHtml;
